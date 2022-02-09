e45521d08c755ac49c104841738c228c0920d235
Lucy Pacius <lucy.pacius@gmail.com>
2022-02-09 14:39:40 -0500

    created new project crown clothing

---
+++ src/homepage.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./homepage.scss";
+
+const HomePage = () => (
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">HATS</h1>
+                    <span className="sub-title">SHOP NOW</span>
+                </div>
+            </div>{/* MENU */}
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">JACKETS</h1>
+                    <span className="sub-title">SHOP NOW</span>
+                </div>
+            </div>{/* MENU */}
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">SNEAKERS</h1>
+                    <span className="sub-title">SHOP NOW</span>
+                </div>
+            </div>{/* MENU */}
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">WOMENS</h1>
+                    <span className="sub-title">SHOP NOW</span>
+                </div>
+            </div>{/* MENU */}
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">MENS</h1>
+                    <span className="sub-title">SHOP NOW</span>
+                </div>
+            </div>{/* MENU */}
+        </div>
+    </div>
+)
+
+export default HomePage;
