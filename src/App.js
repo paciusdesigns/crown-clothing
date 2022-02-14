5d4290f94c1ff43d79d9de83dde23ccaa494aef3
Lucy Pacius <lucy.pacius@gmail.com>
2022-02-13 19:57:01 -0500

    created collection preview shop page

--- src/App.js
+++ src/App.js
@@ -2,34 +2,8 @@ import React from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
 
+import ShopPage from './pages/shop/shop';
 import HomePage from './pages/homepage/homepage';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
-
-const JacketsPage = () => (
-  <div>
-    <h1>JACKETS PAGE</h1>
-  </div>
-);
-const SneakersPage = () => (
-  <div>
-    <h1>SNEAKERS PAGE</h1>
-  </div>
-);
-const WomensPage = () => (
-  <div>
-    <h1>WOMENS PAGE</h1>
-  </div>
-);
-const MensPage = () => (
-  <div>
-    <h1>MENS PAGE</h1>
-  </div>
-);
 
 function App() {
   return (
@@ -37,11 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route exact path='/' element={<HomePage />} />
-          <Route path='/hats' element={<HatsPage />} />
-          <Route path='/jackets' element={<JacketsPage />} />
-          <Route path='/sneakers' element={<SneakersPage />} />
-          <Route path='/womens' element={<WomensPage />} />
-          <Route path='/mens' element={<MensPage />} />
+          <Route path='/shop' element={<ShopPage />} />
         </Routes>
       </BrowserRouter>
     </div>
